b61a420ca36718f2562260d3ff70afc08d507814
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2025-03-14 11:54:48 -0300

    first commit

---
+++ src/main/java/com/santos/quiz_drummond/QuizDrummondApplication.java
@@ -0,0 +1,13 @@
+package com.santos.quiz_drummond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizDrummondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizDrummondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santos/quiz_drummond/QuizDrummondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.quiz_drummond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizDrummondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
